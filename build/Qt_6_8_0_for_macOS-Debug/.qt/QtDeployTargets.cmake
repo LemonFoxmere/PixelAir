e63bf178eb133ec83969746bde5362b3aa6b9181
Lemon Foxmere <lemon@lemonfoxmere.com>
2024-12-22 15:52:54 -0800

    cleaned build

--- build/Qt_6_8_0_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appPixelAir_FILE /Users/lemonfoxmere/qt-projects/PixelAir/build/Qt_6_8_0_for_macOS-Debug/appPixelAir.app/Contents/MacOS/appPixelAir)
-set(__QT_DEPLOY_TARGET_appPixelAir_TYPE EXECUTABLE)
